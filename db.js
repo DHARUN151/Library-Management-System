3ea082ed474e2afb92382a8e3f7e98a10bf3a1b7
dharun151 <sdkkrr2006@gmail.com>
2025-09-28 11:19:44 +0530

    Added .env

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 //connect to mongo db
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function connection() {
-    const connection = await mongoose.connect(process.env.connection);
-    console.log("connected to db");
-
-    
+  console.debug("**********");
+  console.log(process.env);
+  const connection = await mongoose.connect(process.env.CONNECTION_SRTRING);
+  console.log("connected to db");
 }
 
-
 module.exports = connection;
-
-
--- server.js
+++ server.js
@@ -5,10 +5,13 @@ connectDb();
 const Book = require("./bookSchema");
 const app = express();
 const PORT = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(express.json());
-
+// app.use(dotenv);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  // console.log(process.env.connection);
 });
 
 app.post("/addBook", async (req, res) => {
